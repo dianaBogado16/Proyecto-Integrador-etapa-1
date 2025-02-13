d8418a9377aaa5cc847b29086c4422f83f1355b2
Diana BOGADO <dianadiazogado@gmail.com>
2025-02-13 18:43:07 -0300

    Agregue productos a la pagina

--- src/db/productos.js
+++ src/db/productos.js
@@ -2,38 +2,66 @@ const productos = [
     { 
       id: 1, 
       nombre: "Iphone 11 Pro Max", 
-      foto: "imgs/apple-iphone-11-pro-max.jpg", 
+      foto: "imgs/iphone11-rojo.avif", 
       descripcion: "Iphone 11 super pro max", 
       precio: 222.20
     },
-    { 
+    {
       id: 2, 
-      nombre: "IPhone 12", 
-      foto: "imgs/apple-iphone-12.jpg", 
-      descripcion: "Iphone 12, flojito", 
+      nombre: "Notebook HP Pavilion", 
+      foto: "imgs/hp-azul.webp", 
+      descripcion: "Alto drone", 
+      precio: 555.50
+    },
+
+    { 
+      id: 3, 
+      nombre: "Samsung S24 Ultra", 
+      foto: "imgs/s24.jpg", 
+      descripcion: "lorem", 
       precio: 333.30
     },
     { 
-      id: 3, 
+      id: 4, 
       nombre: "Camara Canon", 
       foto: "imgs/camara-canon.jpg", 
       descripcion: "Camara ultra 4k", 
       precio: 444.40
     },
-    { 
-      id: 4, 
-      nombre: "Drone DJI", 
-      foto: "imgs/drone-dji.jpg", 
-      descripcion: "Alto drone", 
-      precio: 555.50
-    },
+
     { 
       id: 5, 
-      nombre: "Auriculares Sony", 
-      foto: "imgs/auriculares-sony.jpg", 
+      nombre: "Auriculares Samsung Bud", 
+      foto: "imgs/auris-bud.avif", 
       descripcion: "Cancelación activa!", 
       precio: 666.60
+    },
+
+    { 
+      id: 6, 
+      nombre: "Televisor LG", 
+      foto: "imgs/tv-LG.webp", 
+      descripcion: "Cancelación activa!", 
+      precio: 666.60
+    },
+
+    { 
+      id: 7, 
+      nombre: "Auriculares Bose", 
+      foto: "imgs/auris-bose.jpg", 
+      descripcion: "Cancelación activa!", 
+      precio: 660.60
+    } ,
+
+    { 
+      id: 8, 
+      nombre: "Aspiradora Gadnic", 
+      foto: "imgs/aspiradora-gadnic.webp", 
+      descripcion: "Cancelación activa!", 
+      precio: 660.60
     }
+    
+
 ]
 
 export default productos    /* Lo exporto para poder usarlo en otro archivo */
