2a0cfddaf118bca597fa101dbc899b239fef94f7
Diana BOGADO <dianadiazogado@gmail.com>
2025-02-13 20:29:11 -0300

    Agregue un logo al nombre del local y modifique los colores de las cartas de los productos

--- src/db/productos.js
+++ src/db/productos.js
@@ -3,14 +3,14 @@ const productos = [
       id: 1, 
       nombre: "Iphone 11 Pro Max", 
       foto: "imgs/iphone11-rojo.avif", 
-      descripcion: "Iphone 11 super pro max", 
+      descripcion: "Iphone 11 pro max. Pantalla: Super Retina XDR OLED de 6.5 pulgadas. Memoria RAM: 4GB. Batería: 3969 mAh con carga rápida (18W) .", 
       precio: 222.20
     },
     {
       id: 2, 
       nombre: "Notebook HP Pavilion", 
       foto: "imgs/hp-azul.webp", 
-      descripcion: "Alto drone", 
+      descripcion: "Pantalla: Varía entre 14. Procesador: Intel Core i5. Memoria RAM:16GB. Sistema operativo: Windows 11 Home.", 
       precio: 555.50
     },
 
@@ -18,22 +18,22 @@ const productos = [
       id: 3, 
       nombre: "Samsung S24 Ultra", 
       foto: "imgs/s24.jpg", 
-      descripcion: "lorem", 
+      descripcion: "Pantalla:6.8 pulgadas con resolución QHD. Memoria RAM: 12GB. Resistencia al agua: Certificación IP68.", 
       precio: 333.30
     },
     { 
       id: 4, 
-      nombre: "Camara Canon", 
-      foto: "imgs/camara-canon.jpg", 
-      descripcion: "Camara ultra 4k", 
+      nombre: "Mouse Logitech", 
+      foto: "imgs/mouse-logitech.webp", 
+      descripcion: "Diseño: Trackball ergonómico con ángulo de inclinación de 20 grados. Inalámbrico con alcance de 10 metros.", 
       precio: 444.40
     },
 
     { 
       id: 5, 
-      nombre: "Auriculares Samsung Bud", 
+      nombre: "Auriculares Samsung Galaxy Bud", 
       foto: "imgs/auris-bud.avif", 
-      descripcion: "Cancelación activa!", 
+      descripcion: "Conectividad: Bluetooth 5.0 con conexión múltiple. Micrófonos: 2 micrófonos externos y 1 interno. Cancelación de ruido, resistencia IPX2, compatibilidad con Android e iOS", 
       precio: 666.60
     },
 
@@ -41,7 +41,7 @@ const productos = [
       id: 6, 
       nombre: "Televisor LG", 
       foto: "imgs/tv-LG.webp", 
-      descripcion: "Cancelación activa!", 
+      descripcion: "Pantalla: LED de 47 pulgadas. Resolución: Full HD. Tecnologia 3D, Smart TV y Wi-Fi incorporado", 
       precio: 666.60
     },
 
@@ -49,7 +49,7 @@ const productos = [
       id: 7, 
       nombre: "Auriculares Bose", 
       foto: "imgs/auris-bose.jpg", 
-      descripcion: "Cancelación activa!", 
+      descripcion: "Cancelación de ruido: Activa, con tres niveles de ajuste. Conectividad: Bluetooth y cable. Batería: Hasta 20 horas de duración en modo inalámbrico. ", 
       precio: 660.60
     } ,
 
@@ -57,7 +57,7 @@ const productos = [
       id: 8, 
       nombre: "Aspiradora Gadnic", 
       foto: "imgs/aspiradora-gadnic.webp", 
-      descripcion: "Cancelación activa!", 
+      descripcion: "Modos de limpieza: 4 modos de limpieza programables. Detección de obstáculos: Tecnología de detección inteligente de obstáculos. Superficies: Capaz de limpiar alfombras, pisos de madera y azulejos. Autonomía: Batería de larga duración.", 
       precio: 660.60
     }
     
